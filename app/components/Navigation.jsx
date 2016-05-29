2f7cc1252a066a6301b55c655cb6614f2a3c9e69
Adam <AdamBCo@me.com>
2016-05-29 09:40:50 -0700

    Initial Commit

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -2,46 +2,26 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router')
 
 var Navigation = React.createClass({
-  onSearch: function (e) {
-    e.preventDefault();
-
-    var location = this.refs.location.value;
-    var encodedLocation = encodeURIComponent(location);
-
-    if (location.length > 0) {
-      this.refs.location.value = "";
-      window.location.hash = '#/?location=' + encodedLocation;
-    }
-
-  },
   render : function () {
     return (
       <div className="top-bar">
         <div className="top-bar-left">
           <ul className="menu">
-            <li className="menu-text">React Weather</li>
+            <li className="menu-text">React Timer</li>
             <li>
-              <IndexLink to="/" activeClassName="active">Get Weather</IndexLink>
+              <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
             </li>
             <li>
-              <Link to="/about" activeClassName="active">About</Link>
-            </li>
-            <li>
-              <Link to="/examples" activeClassName="active">Examples</Link>
+              <Link to="/Countdown" activeClassName="active-link">Countdown</Link>
             </li>
           </ul>
         </div>
         <div className="top-bar-right">
-          <form onSubmit={this.onSearch}>
-            <ul className="menu">
-              <li>
-                <input type="search" ref="location" placeholder="Search Weather By City"/>
-                </li>
-              <li>
-                <button type="submit" className="button" value="Get Weather">Search</button>
-              </li>
-            </ul>
-          </form>
+          <ul className="menu">
+            <li className="menu-text">
+              Created by <a href="http://adambcooper.com" target="_blank">Adam Cooper</a>
+            </li>
+          </ul>
         </div>
       </div>
     );
