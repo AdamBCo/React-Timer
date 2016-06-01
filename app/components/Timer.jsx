f98ec2d0c05811d19a580a9a9ee4bc4426b2cca6
Adam <AdamBCo@me.com>
2016-06-01 06:19:40 -0700

    Added timer functionaily with working tests

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,12 +1,66 @@
 var React = require('react');
 var Clock = require('Clock');
+var CountdownForm = require('CountdownForm');
+
+var Controls = require('Controls');
 
 var Timer = React.createClass({
-  render : function () {
+  getInitialState: function () {
+    return {
+      count: 0,
+      countdownStatus: 'paused'
+    };
+  },
+  componentDidUpdate : function (prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+        this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({count:0});
+        case 'paused':
+          clearInterval(this.timer)
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  startTimer: function () {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count + 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      });
+
+      if (newCount === 0) {
+        this.setState({countdownStatus:'stopped'})
+      }
+    }, 1000);
+  },
+  handleSetCountdown: function (seconds) {
+    this.setState({
+      count: seconds,
+      countdownStatus: 'started'
+    });
+  },
+  handleStatusChange: function (newStatus) {
+    this.setState({
+      countdownStatus: newStatus
+    });
+  },
+  componentWillUnmount: function () {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
+  render: function () {
+    var {count, countdownStatus} = this.state;
+
     return (
       <div>
-        <h1>Timer</h1>
-        <Clock />
+        <h1 className='title'>Timer</h1>
+        <Clock totalSeconds={count}/>
+        <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>
       </div>
     );
   }
---
+++ app/tests/components/timer.test.jsx
@@ -0,0 +1,52 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Timer = require('Timer');
+
+describe('Timer', () => {
+  it('should exist', () => {
+    expect(Timer).toExist();
+  });
+
+  describe('handleSetCountdown', () => {
+    it('should set state to started and count up', (done) => {
+      var timer = TestUtils.renderIntoDocument(<Timer/>);
+      timer.handleSetTimer(0);
+
+      expect(timer.state.count).toBe(0);
+      expect(timer.state.countdownStatus).toBe('started');
+
+      setTimeout(() => {
+        expect(timer.state.count).toBe(1);
+        done();
+      }, 1001);
+    });
+
+    it('should pause countdown on paused status', (done) => {
+      var timer = TestUtils.renderIntoDocument(<Timer/>);
+      timer.handleSetTimer(0);
+      timer.handleStatusChange('paused');
+
+      setTimeout(() => {
+        expect(timer.state.count).toBe(3);
+        expect(timer.state.countdownStatus).toBe('paused');
+        done();
+      }, 1001);
+    });
+
+    it('should reset countdown on stopped status', (done) => {
+      var timer = TestUtils.renderIntoDocument(<Timer/>);
+      timer.handleSetTimer(3);
+      timer.handleStatusChange('stopped');
+
+      setTimeout(() => {
+        expect(timer.state.count).toBe(0);
+        expect(timer.state.countdownStatus).toBe('stopped');
+        done();
+      }, 1001);
+    });
+  });
+});
